4c517eef66b9e4f540530ce95a14fd419ab52721
dpakalarry <devakusa@gmail.com>
2019-10-23 23:52:56 -0400

    Table gets updated with query and enter key is binded to validateForm

--- query.js
+++ query.js
@@ -10,7 +10,33 @@ var site = " http://jservice.io/api/categories/?count=100";
 function parseJSON(promise){
     promise.then(response=>response.json())
     .then(data=> {
-        console.log(data);
+        // console.log(data);
+        var table = document.getElementsByName("qRes")[0];
+        var tabLen = table.rows.length;
+        console.log(tabLen);
+        for(var i = 1; i < tabLen; i++){
+            table.deleteRow(-1);
+        }
+        
+
+        for(var q in data){
+            console.log(data[q]);
+            var row = table.insertRow(1);
+            
+            var date = row.insertCell(0);
+            var category = row.insertCell(1);
+            var quest = row.insertCell(2);
+            var answ = row.insertCell(3);
+            var save = row.insertCell(4);
+
+            const actDate = new Date(data[q].airdate)
+
+            date.innerHTML = "<h6>"+ actDate.toLocaleDateString() + "</h6>";
+            category.innerHTML = "<h6>"+ data[q].category.title + "</h6>";
+            quest.innerHTML = "<h6>"+ data[q].question + "</h6>";
+            answ.innerHTML = "<h6>"+ data[q].answer + "</h6>";
+            save.innerHTML = "<input type = \"checkbox\">";
+        }
     });
 }
 
@@ -48,25 +74,31 @@ function validateForm(numRes = 100) {
         document.getElementsByName("dateMin")[0].focus();
         return false;
     } 
-    if(maxDate == "" && minDate == "" && price == "" && cat == ""){
+    if(numRes != 100 && maxDate == "" && minDate == "" && price == "" && cat == ""){
         alert("No fields were filled out");
         document.getElementsByName("dateMin")[0].focus();
         return false;
     }
-    var site = " http://jservice.io/api/clues/?";
-    if(price != ""){
-        site += "value=" + price + "&"
+    var site = "http://jservice.io/api/"
+    if(numRes == 100){
+        site += "clues/?";
+        if(price != ""){
+            site += "value=" + price + "&"
+        }
+        if(cat != ""){
+            site += "category=" + cat + "&"
+        }
+        if(minDate != ""){
+            site += "min_date=" + minDate + "&"
+        }
+        if(maxDate != ""){
+            site += "max_date=" + maxDate + "&"
+        }
+        site = site.substr(0,site.length-1);
+    } else{
+        site += "random/?count=1"
     }
-    if(cat != ""){
-        site += "category=" + cat + "&"
-    }
-    if(minDate != ""){
-        site += "min_date=" + minDate + "&"
-    }
-    if(maxDate != ""){
-        site += "max_date=" + maxDate + "&"
-    }
-    site = site.substr(0,site.length-1);
+    
     console.log(site);
     var prom = getJSON(site);
     parseJSON(prom)
@@ -74,3 +106,10 @@ function validateForm(numRes = 100) {
 
 
 }
+
+
+window.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+      validateForm();
+    }
+  });
